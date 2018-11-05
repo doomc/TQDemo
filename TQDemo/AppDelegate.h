d03af59d2e701463024866dc22819e447183f4ee
doomc <dong9292110@163.com>
2018-11-05 09:35:02 +0800

    Initial Commit

---
+++ TQDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TQDemo
+//
+//  Created by doomc on 2018/11/5.
+//  Copyright © 2018 DoomC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TQDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TQDemo
+//
+//  Created by doomc on 2018/11/5.
+//  Copyright © 2018 DoomC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
